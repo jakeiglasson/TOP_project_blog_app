b2b9b23e712156408578b4ede54c27321fabb94f
Jake Glasson <jakeiglasson@gmail.com>
2020-04-21 20:18:29 +1000

    first blogger commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # resources :articles
+  
+  get     "/articles",          to: "articles#index", as: "articles"      #index
+  post    "/articles",          to: "articles#create"                     #create
+  get     "/articles/new",      to: "articles#new",   as: "new_article"   #new
+  get     "/articles/:id/edit", to: "articles#edit",  as: "edit_article"  #edit
+  get     "/articles/:id",      to: "articles#show",  as: "article"       #show
+  patch   "/articles/:id",      to: "articles#update"                     #update
+  put     "/articles/:id",      to: "articles#update"                     #update
+  delete  "/articles/:id",      to: "articles#destroy"                    #destroy
+
+
+end
